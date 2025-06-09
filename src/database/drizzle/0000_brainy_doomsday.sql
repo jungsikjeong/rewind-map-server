c3203da80d655725b24ea4b83369ce074c8eebbb
jungsikJeong <wndtlr1024@gmail.com>
2025-06-09 13:48:49 +0900

    refactor: 불필요한 스키마 파일 삭제 및 감정 태그 테이블 추가, 게시물 스키마 수정

--- src/database/drizzle/0000_brainy_doomsday.sql
+++ src/database/drizzle/0000_brainy_doomsday.sql
@@ -1,3 +1,11 @@
+CREATE TABLE "emotion_tags" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"emoji" text,
+	"color" text,
+	"description" text
+);
+--> statement-breakpoint
 CREATE TABLE "likes" (
 	"id" uuid PRIMARY KEY NOT NULL,
 	"post_id" uuid NOT NULL,
@@ -13,10 +21,11 @@ CREATE TABLE "posts" (
 	"is_published" boolean DEFAULT false,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now(),
-	"author_id" integer NOT NULL,
+	"author_id" uuid NOT NULL,
 	"latitude" text NOT NULL,
 	"longitude" text NOT NULL,
-	"emotion_tag" text NOT NULL
+	"emotion_tag_id" uuid NOT NULL,
+	"revisit_at" timestamp
 );
 --> statement-breakpoint
 CREATE TABLE "profile" (
@@ -28,12 +37,14 @@ CREATE TABLE "profile" (
 --> statement-breakpoint
 CREATE TABLE "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"email" text,
-	"nickname" text,
-	"password" text,
+	"email" text NOT NULL,
+	"nickname" text NOT NULL,
+	"password" text NOT NULL,
+	"role" text DEFAULT 'user' NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email"),
 	CONSTRAINT "users_nickname_unique" UNIQUE("nickname")
 );
 --> statement-breakpoint
 ALTER TABLE "likes" ADD CONSTRAINT "likes_post_id_posts_id_fk" FOREIGN KEY ("post_id") REFERENCES "public"."posts"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "posts" ADD CONSTRAINT "posts_emotion_tag_id_emotion_tags_id_fk" FOREIGN KEY ("emotion_tag_id") REFERENCES "public"."emotion_tags"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "profile" ADD CONSTRAINT "profile_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
